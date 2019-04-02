cfab7457b3e93ac8cf80207d4a691b62a550445e
Jonti Hudson <j.hudson@grillaz.uk>
2019-04-02 11:55:01 +0100

    getters and setters added for state

--- flow.js
+++ flow.js
@@ -9,10 +9,34 @@ export type ValidatorType = {
 
 export type OptionsType = {
   validator?: ValidatorType,
-  debugMode?: boolean
+  debugMode?: boolean,
+  encryptionKey?: string
 };
 
 export type ComponentType = Component<any, any> | PureComponent<any, any>;
 
 type FunctionType<T> = T => T;
 export type AllowFunctionType = <V>(V) => FunctionType<V> | V;
+
+export interface SharedStateType<StateType: Object> {
+  +state: StateType;
+  +prevState: StateType;
+
+  setState(partialState: $Shape<StateType>, callback?: () => any): void;
+  reset(): void;
+
+  register(
+    component: ComponentType,
+    updateKeys: $Keys<StateType> | Array<$Keys<StateType>>
+  ): void;
+  unregister(component: ComponentType): void;
+
+  useState<K: $Keys<StateType>>(
+    propKey: K
+  ): [$ElementType<StateType, K>, ($ElementType<StateType, K>) => void];
+
+  useStorage(storeName: string): Promise<boolean>;
+  save(): boolean;
+
+  toString(): string;
+}
