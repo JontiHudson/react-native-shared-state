3635b92ecae3b9e6b4bb869199b77dfd528b015b
Jonti Hudson <j.hudson@grillaz.uk>
2019-09-13 20:57:10 +0100

    version 0.7.0 - flow removed, typescript types added

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "airbnb",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'class-methods-use-this': 1,
+    'guard-for-in': 0,
+    'import/no-unresolved': 1,
+    'no-continue': 0,
+    'no-empty': 1,
+    'no-labels': 0,
+    'no-plusplus': 0,
+    'no-restricted-globals': 1,
+    'no-restricted-syntax': 0,
+    'no-undef': 1,
+    'no-underscore-dangle': 0,
+    'no-unused-expressions': 0,
+    'valid-typeof': 0,
+  },
 };
--- index.js
+++
@@ -1,3 +0,0 @@
-export { default as SharedMap } from "./lib/SharedMap";
-export { default as SharedState } from "./lib/SharedState";
-export * from "./lib/helpers";
